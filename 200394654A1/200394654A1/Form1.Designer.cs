45115112850d588c2298d32fb68d8fdf43ea5894
Mikelvar <200394654@student.georgianc.on.ca>
2020-03-01 09:42:43 -0500

    Add files via upload

---
+++ 200394654A1/200394654A1/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace _200394654A1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Quarters = new System.Windows.Forms.TextBox();
+            this.Dimes = new System.Windows.Forms.TextBox();
+            this.Loonies = new System.Windows.Forms.TextBox();
+            this.Toonies = new System.Windows.Forms.TextBox();
+            this.Nickels = new System.Windows.Forms.TextBox();
+            this.Total = new System.Windows.Forms.TextBox();
+            this.Paide = new System.Windows.Forms.TextBox();
+            this.Change = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Clear_button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Quarters
+            // 
+            this.Quarters.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Quarters.Enabled = false;
+            this.Quarters.Location = new System.Drawing.Point(146, 80);
+            this.Quarters.Name = "Quarters";
+            this.Quarters.Size = new System.Drawing.Size(60, 20);
+            this.Quarters.TabIndex = 2;
+            this.Quarters.TextChanged += new System.EventHandler(this.Quarters_TextChanged);
+            // 
+            // Dimes
+            // 
+            this.Dimes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Dimes.Enabled = false;
+            this.Dimes.Location = new System.Drawing.Point(212, 80);
+            this.Dimes.Name = "Dimes";
+            this.Dimes.Size = new System.Drawing.Size(60, 20);
+            this.Dimes.TabIndex = 3;
+            this.Dimes.TextChanged += new System.EventHandler(this.Dimes_TextChanged);
+            // 
+            // Loonies
+            // 
+            this.Loonies.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Loonies.Enabled = false;
+            this.Loonies.Location = new System.Drawing.Point(80, 80);
+            this.Loonies.Name = "Loonies";
+            this.Loonies.Size = new System.Drawing.Size(60, 20);
+            this.Loonies.TabIndex = 4;
+            this.Loonies.TextChanged += new System.EventHandler(this.Loonies_TextChanged);
+            // 
+            // Toonies
+            // 
+            this.Toonies.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Toonies.Enabled = false;
+            this.Toonies.Location = new System.Drawing.Point(14, 80);
+            this.Toonies.Name = "Toonies";
+            this.Toonies.Size = new System.Drawing.Size(60, 20);
+            this.Toonies.TabIndex = 6;
+            this.Toonies.TextChanged += new System.EventHandler(this.Toonies_TextChanged);
+            // 
+            // Nickels
+            // 
+            this.Nickels.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Nickels.Enabled = false;
+            this.Nickels.Location = new System.Drawing.Point(278, 80);
+            this.Nickels.Name = "Nickels";
+            this.Nickels.Size = new System.Drawing.Size(60, 20);
+            this.Nickels.TabIndex = 7;
+            this.Nickels.TextChanged += new System.EventHandler(this.Nickels_TextChanged);
+            // 
+            // Total
+            // 
+            this.Total.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Total.Location = new System.Drawing.Point(13, 26);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(64, 20);
+            this.Total.TabIndex = 8;
+            this.Total.TextChanged += new System.EventHandler(this.Total_TextChanged);
+            // 
+            // Paide
+            // 
+            this.Paide.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Paide.Location = new System.Drawing.Point(84, 26);
+            this.Paide.Name = "Paide";
+            this.Paide.Size = new System.Drawing.Size(64, 20);
+            this.Paide.TabIndex = 9;
+            this.Paide.TextChanged += new System.EventHandler(this.Paide_TextChanged);
+            // 
+            // Change
+            // 
+            this.Change.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Change.Enabled = false;
+            this.Change.Location = new System.Drawing.Point(154, 26);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(64, 20);
+            this.Change.TabIndex = 10;
+            this.Change.TextChanged += new System.EventHandler(this.Change_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Paid";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Change";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Toonies";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Loonies";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(151, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Quarters";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(209, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Dimes";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(275, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Nickels";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Total";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 149);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.Paide);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.Dimes);
+            this.Controls.Add(this.Nickels);
+            this.Controls.Add(this.Toonies);
+            this.Controls.Add(this.Loonies);
+            this.Controls.Add(this.Quarters);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Quarters;
+        private System.Windows.Forms.TextBox Dimes;
+        private System.Windows.Forms.TextBox Loonies;
+        private System.Windows.Forms.TextBox Toonies;
+        private System.Windows.Forms.TextBox Nickels;
+        private System.Windows.Forms.TextBox Total;
+        private System.Windows.Forms.TextBox Paide;
+        private System.Windows.Forms.TextBox Change;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ 200394654A1/200394654A1/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _200394654A1
+{
+    public partial class Form1 : Form
+    {
+         
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        ///theas methodes alow the user to clear and calculate the change due 
+        ///********************************************************************
+        ///clear info
+        private void Clear_button_Click(object sender, EventArgs e)
+        {
+            ///this alows us to clear all the info from are previose calculation
+            
+            ///this will clear the Quarters textbox
+            this.Quarters.Text = string.Empty;
+
+            ///this will clear the Dimes textbox
+            this.Dimes.Text = string.Empty;
+
+            ///this will clear the Loonies textbox
+            this.Loonies.Text = string.Empty;
+
+            ///this will clear the Toonies textbox
+            this.Toonies.Text = string.Empty;
+
+            ///this will clear the Nickels textbox
+            this.Nickels.Text = string.Empty;
+
+            ///this will clear the Paide textbox
+            this.Paide.Text = string.Empty;
+
+            ///this will clear the Total textbox
+            this.Total.Text = string.Empty;
+
+            ///this will clear the Change textbox
+            this.Change.Text = string.Empty;
+
+        }
+        /// Calculate Change due
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            ///this calculate the amount of coings needed to make the change
+
+            ///this calculates the change for the amount of coings needed 
+            double change;
+            change = Double.Parse(Paide.Text) - Double.Parse(Total.Text);
+
+            ///this calculate the amount of toonies needed
+            double toonies = change / 2;
+            change %= 2;
+
+            ///this calculate the amount of loonies needed
+            double loonies = change / 1;
+            change %= 1;
+
+            ///this calculate the amount of quarters needed
+            double quarters = change / 0.25;
+            change %= 0.25;
+
+            ///this calculate the amount of dimes needed
+            double dimes = change / 0.10;
+            change %= 0.10;
+
+            ///this calculate the amount of nickels needed
+            double nickels = change / 0.05;
+
+            ///checks if the text box is empty 
+            if (String.IsNullOrEmpty(Paide.Text) || String.IsNullOrEmpty(Total.Text))
+            {///this will dispaly hte message if necessary
+                MessageBox.Show("you must fill in the info");
+            }
+            else
+            {   ///Calculate Change due                
+                Change.Text = (Double.Parse(Paide.Text) - Double.Parse(Total.Text)).ToString();
+                this.Change.Text = this.Change.Text.Insert(0, "$");
+                
+                ///prints out the correct number of coins to give the user
+               
+                ///this prinst the amount of toonies
+                Toonies.Text += + Math.Floor(toonies);
+                
+                ///this prinst the amount of toonies
+                Loonies.Text += + Math.Floor(loonies);
+                
+                ///this prinst the amount of toonies
+                Quarters.Text += + Math.Floor(quarters);
+               
+                ///this prinst the amount of toonies
+                Dimes.Text +=  + Math.Floor(dimes);
+               
+                ///this prinst the amount of toonies
+                Nickels.Text += + Math.Round(nickels);
+                
+                ///this checks if the amout paide was sufficient
+                if (Double.Parse(Paide.Text) - Double.Parse(Total.Text) < 0)
+                {///this will dispaly hte message if necessary
+                    MessageBox.Show("Please give the total that is due!!!!!");
+                }
+
+               
+            }
+
+
+
+          
+
+        }
+        ///this is theere the user enter the total
+        private void Total_TextChanged(object sender, EventArgs e)
+        {
+            ///checks if the user input a value other then a number 
+            if (System.Text.RegularExpressions.Regex.IsMatch(Total.Text, "[^0.-9]"))
+                {///this will dispaly hte message if necessary
+                MessageBox.Show("you can only enter numbers.");
+                Total.Text = Total.Text.Remove(Total.Text.Length - 1);
+                }
+
+          
+
+        }
+         ///this is where the user enters the amout that is paide 
+        private void Paide_TextChanged(object sender, EventArgs e)
+        {///checks if the user input a value other then a number 
+            if (System.Text.RegularExpressions.Regex.IsMatch(Paide.Text, "[^0.-9]"))
+            {///this will dispaly hte message if necessary
+                MessageBox.Show("you can only enter numbers.");
+                Paide.Text = Paide.Text.Remove(Paide.Text.Length - 1);
+            }
+
+           
+        }
+        
+        private void Toonies_TextChanged(object sender, EventArgs e)
+        {
+           
+           
+
+
+        }
+        private void Loonies_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void Quarters_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void Dimes_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Nickels_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Change_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
